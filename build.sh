46a229a7828092d3cfecdc8cf957bb14f023af93
Max Walters <mdwalters.pm@proton.me>
2025-01-26 12:36:44 -0500

    install gnome apps

--- build.sh
+++ build.sh
@@ -14,7 +14,8 @@ dnf install -y \
 
 # install environment packages 
 dnf install -y \
-	plasma-desktop
+	plasma-desktop \
+	dconf gnome-boxes gnome-connections gnome-settings-daemon gnome-software ptyxis gnome-text-editor nautilus polkit yelp ModemManager NetworkManager-adsl NetworkManager-openconnect-gnome NetworkManager-openvpn-gnome NetworkManager-ppp NetworkManager-pptp-gnome NetworkManager-ssh-gnome NetworkManager-vpnc-gnome NetworkManager-wwan PackageKit-command-not-found PackageKit-gtk3-module avahi baobab evince evince-djvu fprintd-pam glib-networking gnome-backgrounds gnome-bluetooth gnome-browser-connector gnome-calculator gnome-calendar gnome-characters gnome-clocks gnome-color-manager gnome-contacts gnome-tweaks gnome-disk-utility gnome-font-viewer gnome-logs gnome-maps gnome-remote-desktop gnome-session-xsession gnome-system-monitor gnome-user-share gnome-weather gvfs-afc gvfs-afp gvfs-archive gvfs-fuse gvfs-goa gvfs-gphoto2 gvfs-mtp gvfs-smb librsvg2 libsane-hpaio loupe mesa-dri-drivers mesa-libEGL rygel sane-backends-drivers-scanners simple-scan snapshot sushi systemd-oomd-defaults vlc tracker tracker-miners xdg-desktop-portal xdg-desktop-portal-gnome xdg-desktop-portal-gtk xdg-user-dirs-gtk
 
 # install user packages
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
