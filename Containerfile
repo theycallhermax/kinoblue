d876293b95281144c2ac0e5a240c097df33c17d4
Max Walters <mdwalters.pm@proton.me>
2025-01-26 13:03:24 -0500

    change base image to `ghcr.io/ublue-os/base-main:41` add fonts add glib overrides add yaru sound theme

--- Containerfile
+++ Containerfile
@@ -1,9 +1,13 @@
-FROM quay.io/fedora/fedora-bootc:41
+FROM ghcr.io/ublue-os/base-main:41
 
 COPY build.sh /tmp/build.sh
 
 RUN rm -rf dotfiles/.git && mkdir -p /usr/etc/skel
 ADD dotfiles /usr/etc/skel
+
+RUN dnf install -y glib2
+ADD overrides /usr/share/glib-2.0/schemas/
+RUN glib-compile-schemas /usr/share/glib-2.0/schemas
 
 RUN mkdir -p /var/lib/alternatives && \
 	/tmp/build.sh && \
--- build.sh
+++ build.sh
@@ -10,11 +10,15 @@ dnf -y copr enable sneexy/zen-browser
 # install cli packages
 dnf install -y \
 	flatpak \
- 	git
+ 	git \
+    fuse fuse-libs
 
 # install environment packages 
 dnf install -y \
 	plasma-desktop \
+	rsms-inter-fonts \
+	cascadia-code-fonts cascadia-code-nf-fonts cascadia-code-pl-fonts \
+	yaru-sound-theme
 	dconf gnome-boxes gnome-connections gnome-settings-daemon gnome-software ptyxis gnome-text-editor nautilus polkit yelp ModemManager NetworkManager-adsl NetworkManager-openconnect-gnome NetworkManager-openvpn-gnome NetworkManager-ppp NetworkManager-pptp-gnome NetworkManager-ssh-gnome NetworkManager-vpnc-gnome NetworkManager-wwan PackageKit-command-not-found PackageKit-gtk3-module avahi baobab evince evince-djvu fprintd-pam glib-networking gnome-backgrounds gnome-bluetooth gnome-browser-connector gnome-calculator gnome-calendar gnome-characters gnome-clocks gnome-color-manager gnome-contacts gnome-tweaks gnome-disk-utility gnome-font-viewer gnome-logs gnome-maps gnome-remote-desktop gnome-session-xsession gnome-system-monitor gnome-user-share gnome-weather gvfs-afc gvfs-afp gvfs-archive gvfs-fuse gvfs-goa gvfs-gphoto2 gvfs-mtp gvfs-smb librsvg2 libsane-hpaio loupe mesa-dri-drivers mesa-libEGL rygel sane-backends-drivers-scanners simple-scan snapshot sushi systemd-oomd-defaults vlc tracker tracker-miners xdg-desktop-portal xdg-desktop-portal-gnome xdg-desktop-portal-gtk xdg-user-dirs-gtk
 
 # install icon theme
