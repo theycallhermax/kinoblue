9bc2921ef57a9d01ef74c1d79aedf608e111ed8c
theycallhermax <mdwalters.pm@proton.me>
2025-01-25 23:53:42 -0500

    add dotfiles

--- Containerfile
+++ Containerfile
@@ -2,7 +2,11 @@ FROM quay.io/fedora/fedora-bootc:41
 
 COPY build.sh /tmp/build.sh
 
+RUN rm -rf dotfiles/.git && mkdir -p /usr/etc/skel
+ADD dotfiles /usr/etc/skel
+ADD --chmod=755 dotfiles/.local/bin /usr/etc/skel/.local/bin
+
 RUN mkdir -p /var/lib/alternatives && \
-    /tmp/build.sh && \
-    ostree container commit && \
-    bootc container lint
+	/tmp/build.sh && \
+	ostree container commit && \
+	bootc container lint
--- build.sh
+++ build.sh
@@ -2,10 +2,16 @@
 
 set -ouex pipefail
 
-dnf install -y --nogpgcheck --repofrompath "terra,https://repos.fyralabs.com/terra$(rpm -E %fedora)" terra-release
+# install repos
 dnf install -y "dnf5-command(copr)"
+dnf install -y --nogpgcheck --repofrompath "terra,https://repos.fyralabs.com/terra$(rpm -E %fedora)" terra-release
 dnf -y copr enable sneexy/zen-browser
 
+# install environment packages 
+dnf install -y \
+	plasma-desktop
+
+# install user packages
 dnf install -y \
 	zen-browser \
 	zed
