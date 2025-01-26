4dd77d14f7dfdc401025e1b2d522d835ef3949c0
theycallhermax <mdwalters.pm@proton.me>
2025-01-25 23:18:46 -0500

    fix copr command

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 set -ouex pipefail
 
 dnf install -y --nogpgcheck --repofrompath "terra,https://repos.fyralabs.com/terra$(rpm -E %fedora)" terra-release
-dnf copr enable -y sneexy/zen-browser
+dnf5 -y copr enable sneexy/zen-browser
 
 dnf install -y \
 	zen \
