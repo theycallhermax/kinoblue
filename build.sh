18a7c1b0fa43df629cf3d1d8d4b53befddb44c9e
Max Walters <mdwalters.pm@proton.me>
2025-01-28 11:21:04 -0500

    remove unneeded packages from environment packafes

--- build.sh
+++ build.sh
@@ -9,10 +9,7 @@ dnf install -y terra-release
 
 # install cli packages
 dnf install -y \
-	flatpak \
-	git \
-	fuse fuse-libs \
-	distrobox
+	git 
 
 # install environment packages 
 dnf install -y \
@@ -20,7 +17,7 @@ dnf install -y \
 	rsms-inter-fonts \
 	cascadia-code-fonts cascadia-code-nf-fonts cascadia-code-pl-fonts \
 	yaru-sound-theme \
-	dconf gnome-boxes gnome-connections gnome-settings-daemon gnome-software ptyxis gnome-text-editor nautilus polkit yelp ModemManager NetworkManager-adsl NetworkManager-openconnect-gnome NetworkManager-openvpn-gnome NetworkManager-ppp NetworkManager-pptp-gnome NetworkManager-ssh-gnome NetworkManager-vpnc-gnome NetworkManager-wwan PackageKit-command-not-found PackageKit-gtk3-module avahi baobab evince evince-djvu fprintd-pam glib-networking gnome-backgrounds gnome-bluetooth gnome-browser-connector gnome-calculator gnome-calendar gnome-characters gnome-clocks gnome-color-manager gnome-contacts gnome-tweaks gnome-disk-utility gnome-font-viewer gnome-logs gnome-maps gnome-remote-desktop gnome-session-xsession gnome-system-monitor gnome-user-share gnome-weather gvfs-afc gvfs-afp gvfs-archive gvfs-fuse gvfs-goa gvfs-gphoto2 gvfs-mtp gvfs-smb librsvg2 libsane-hpaio loupe mesa-dri-drivers mesa-libEGL rygel sane-backends-drivers-scanners simple-scan snapshot sushi systemd-oomd-defaults vlc tracker tracker-miners xdg-desktop-portal xdg-desktop-portal-gnome xdg-desktop-portal-gtk xdg-user-dirs-gtk
+	gnome-boxes gnome-connections gnome-software ptyxis gnome-text-editor nautilus baobab evince evince-djvu gnome-calculator gnome-calendar gnome-characters gnome-clocks gnome-contacts gnome-tweaks gnome-disk-utility gnome-font-viewer gnome-logs gnome-maps gnome-remote-desktop gnome-system-monitor gnome-weather loupe simple-scan snapshot sushi vlc
 
 # install icon theme
 git clone --depth=1 https://github.com/theycallhermax/WhiteSur-icon-theme.git /tmp/WhiteSur-icon-theme
